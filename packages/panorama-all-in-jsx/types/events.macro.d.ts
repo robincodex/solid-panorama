52a4d8ccabd557b57f707fed4524f0e165cf4b5c
RobinCode <xv.robincode@gmail.com>
2022-11-26 14:12:01 +0800

    panorama-all-in-jsx: Add events.macro.d.ts

---
+++ packages/panorama-all-in-jsx/types/events.macro.d.ts
@@ -0,0 +1,8 @@
+export function useGameEvent<
+    T extends keyof GameEventDeclarations,
+    D extends NetworkedData<GameEventDeclarations[T]>
+>(eventName: T, callback: (event: D) => void, deps?: any): void;
+export function useGameEvent<
+    T extends keyof CustomGameEventDeclarations,
+    D extends NetworkedData<CustomGameEventDeclarations[T]>
+>(eventName: T, callback: (event: D) => void, deps?: any): void;
