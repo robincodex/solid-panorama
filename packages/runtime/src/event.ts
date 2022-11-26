9081a72b0dca4ad28a95d6f98ba670c89a2ba39d
RobinCode <xv.robincode@gmail.com>
2022-11-26 13:48:55 +0800

    runtime: Remove useGameEvent

--- packages/runtime/src/event.ts
+++ packages/runtime/src/event.ts
@@ -1,28 +1,4 @@
-import { createEffect } from 'solid-js';
 import { noop } from './utils';
-
-export function useGameEvent<
-    T extends keyof GameEventDeclarations,
-    D extends NetworkedData<GameEventDeclarations[T]>
->(eventName: T, callback: (event: D) => void, deps?: any): void;
-export function useGameEvent<
-    T extends keyof CustomGameEventDeclarations,
-    D extends NetworkedData<CustomGameEventDeclarations[T]>
->(eventName: T, callback: (event: D) => void, deps?: any): void;
-export function useGameEvent(
-    eventName: string,
-    callback: (event: object) => void,
-    deps?: any
-): void {
-    createEffect(() => {
-        const id = GameEvents.Subscribe(eventName, evt => {
-            callback(evt);
-        });
-        return () => {
-            GameEvents.Unsubscribe(id);
-        };
-    }, deps);
-}
 
 export function setDragEvent(
     node: Panel,
